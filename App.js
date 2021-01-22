4a4b1c303cb99d5727b2ab6efd4845354f587d59
Seema Vora <seema.s.vora@gmail.com>
2021-01-22 15:00:57 -0800

    Adds CarItem Component

--- App.js
+++ App.js
@@ -1,18 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View, ImageBackground } from 'react-native';
+import CarItem from './components/CarItem/index';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <View style={styles.carContainer}>
-        <ImageBackground source={require('./assets/images/ModelX.jpeg')} 
-        style = {styles.image}/>
-        <View style={styles.titles}>
-          <Text style={styles.title}>Model X</Text>
-          <Text style={styles.subtitle}>Starting at $69,000</Text>
-        </View>
-      </View>
+      <CarItem/>
       <StatusBar style="auto" />
     </View>
   );
@@ -25,27 +19,5 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
-  carContainer: {
-    width: '100%',
-    height: '100%',
-  },
-  titles: {
-    marginTop: '30%',
-    width: '100%',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 40,
-    fontWeight: '500',
-  },
-  subtitle: {
-    fontSize: 16,
-    color: '#5c5e62'
-  },
-  image: {
-    width:'100%',
-    height:'100%',
-    resizeMode:'cover',
-    position:'absolute',
-  }
+ 
 });
