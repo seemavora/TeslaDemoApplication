3500fa770e0c38317d5b9decefdb5cfaa65fde1f
Seema Vora <seema.s.vora@gmail.com>
2021-01-22 15:58:41 -0800

    Car Items Updated with Underline Text

--- components/CarItem/index.js
+++ components/CarItem/index.js
@@ -3,14 +3,27 @@ import { View, Text, ImageBackground } from 'react-native';
 import styles from './styles';
 import StyledButton from '../StyledButton'
 const CarItem = (props) => {
+
+  const{name, tagline, image, taglineCTA} = props;
   return (
     <View style={styles.carContainer}>
-      <ImageBackground source={require('../../assets/images/ModelX.jpeg')}
-        style={styles.image} />
+      <ImageBackground source={image}
+        style={styles.image}
+        />
+
       <View style={styles.titles}>
-        <Text style={styles.title}>Model X</Text>
-        <Text style={styles.subtitle}>Starting at $69,000</Text>
+        <Text style={styles.title}>{name}</Text>
+        <Text style={styles.subtitle}>
+        {tagline}
+        {' '}
+        <Text style = {styles.subtitleCTA}>
+          {taglineCTA}
+        </Text>
+        </Text>
       </View>
+
+      <View style = {styles.buttonContainer} >
+
       <StyledButton
         type="primary"
         content={"Custom Order"}
@@ -26,6 +39,7 @@ const CarItem = (props) => {
           console.warn("Existing Inventory was pressed");
         }}
       />
+      </View>
     </View>
   );
 };
